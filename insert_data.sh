bc889363ed9541d77706a5103d552292c95b3ec9
Jason Wingert <wingertjaylee@gmail.com>
2025-06-24 03:23:51 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,22 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+# Clear tables
+$PSQL "TRUNCATE games, teams RESTART IDENTITY;"
+
+# Read CSV and process entries
+tail -n +2 games.csv | while IFS=',' read -r YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  # Insert winner team
+  $PSQL "INSERT INTO teams(name) VALUES('$WINNER') ON CONFLICT (name) DO NOTHING;"
+  
+  # Insert opponent team
+  $PSQL "INSERT INTO teams(name) VALUES('$OPPONENT') ON CONFLICT (name) DO NOTHING;"
+  
+  # Get team IDs
+  WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER';")
+  OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT';")
+  
+  # Insert game
+  $PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS);"
+done
